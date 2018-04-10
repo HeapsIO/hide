72b353c819d339400ee58b26c40a06b62509607d
trethaller <tom.rethaller@gmail.com>
2018-04-10 09:16:35 +0200

    Resource: allow loading level3d

--- hide/prefab/Resource.hx
+++ hide/prefab/Resource.hx
@@ -7,8 +7,12 @@ class Resource extends hxd.res.Resource {
 	public function load() : Library {
 		if( lib != null )
 			return lib;
-		lib = new Library();
-		lib.load(haxe.Json.parse(entry.getText()));
+		var data = haxe.Json.parse(entry.getText());
+		if(data.type == "level3d")
+			lib = new hide.prefab.l3d.Level3D();
+		else
+			lib = new Library();
+		lib.load(data);
 		watch(function() lib.reload(haxe.Json.parse(entry.getText())));
 		return lib;
 	}
