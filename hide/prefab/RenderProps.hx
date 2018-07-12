fd7c1063497b06e96242d235443bd4cbf7a978ef
trethaller <tom.rethaller@gmail.com>
2018-07-12 12:51:24 +0200

    RendererProps: fallback to default props when props are missing

--- hide/prefab/RenderProps.hx
+++ hide/prefab/RenderProps.hx
@@ -15,8 +15,34 @@ class RenderProps extends Prefab {
 		return {};
 	}
 
-	public function getProps() {
-		return Reflect.field(this.props, h3d.mat.MaterialSetup.current.name);
+	public function getProps(renderer: h3d.scene.Renderer) {
+		var p = Reflect.field(this.props, h3d.mat.MaterialSetup.current.name);
+		var defaults = renderer.getDefaultProps();
+		function loadProps(from: Dynamic, to: Dynamic) {
+			for(field in Reflect.fields(from)) {
+				var fromVal = Reflect.field(from, field);
+				if(fromVal == null)
+					continue;
+				var toVal = Reflect.field(to, field);
+				if(Type.typeof(fromVal) == TObject) {
+					if(toVal == null) {
+						toVal = {};
+						Reflect.setField(to, field, toVal);
+					}
+					if(Type.typeof(toVal) == TObject) {
+						loadProps(fromVal, toVal);
+					}
+				}
+				else if(toVal == null)
+					Reflect.setField(to, field, fromVal);
+			}
+		}
+		if(p == null) {
+			p = {};
+			Reflect.setField(this.props, h3d.mat.MaterialSetup.current.name, p);
+		}
+		loadProps(defaults, p);
+		return p;
 	}
 
 	public function setProps( props : Any ) {
@@ -28,7 +54,7 @@ class RenderProps extends Prefab {
 	}
 
 	public function applyProps(renderer: h3d.scene.Renderer) {
-		var props = getProps();
+		var props = getProps(renderer);
 		if( props == null )
 			return false;
 		renderer.props = props;
@@ -40,7 +66,7 @@ class RenderProps extends Prefab {
 		super.edit(ctx);
 		#if editor
 		var renderer = ctx.scene.s3d.renderer;
-		var props = getProps();
+		var props = getProps(renderer);
 		var needSet = false;
 		if( props == null ) {
 			props = ctx.ide.parseJSON(ctx.ide.toJSON(renderer.props));
