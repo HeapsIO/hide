11f337dfdb86399368ef510fd1f28cb73b9ca9c7
trethaller <tom.rethaller@gmail.com>
2019-04-05 14:23:15 +0200

    More layout fixes :'(

--- hide/view/l3d/Level3D.hx
+++ hide/view/l3d/Level3D.hx
@@ -331,7 +331,7 @@ class Level3D extends FileView {
 							<label>Favorites</label>
 							<div class="favorites-tree"></div>
 						</div>
-						<div style="height:80%;">
+						<div style="height:80%;" class="flex vertical">
 							<div class="hide-toolbar" style="zoom: 80%">
 								<div class="button collapse-btn" title="Collapse all">
 									<div class="icon fa fa-reply-all"></div>
