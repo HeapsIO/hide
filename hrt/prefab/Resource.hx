744dfea04f62d9faa6d5b9fbda46f07980b2aa92
Nicolas Cannasse <ncannassse@gmail.com>
2019-07-15 17:27:35 +0200

    fixed manual prefab watch() was ignoring lib reload

--- hrt/prefab/Resource.hx
+++ hrt/prefab/Resource.hx
@@ -4,13 +4,21 @@ class Resource extends hxd.res.Resource {
 
 	var lib : Prefab;
 
+	override function watch( onChanged: Null<Void -> Void> ) {
+		if( onChanged == null ) {
+			super.watch(null);
+			return;
+		}
+		super.watch(function() { if( lib != null ) lib.reload(haxe.Json.parse(entry.getText())); onChanged(); });
+	}
+
 	public function load() : Prefab {
 		if( lib != null )
 			return lib;
 		var data = haxe.Json.parse(entry.getText());
 		lib = Library.create(entry.extension);
 		lib.loadData(data);
-		watch(function() lib.reload(haxe.Json.parse(entry.getText())));
+		watch(function() {}); // auto lib reload
 		return lib;
 	}
 
