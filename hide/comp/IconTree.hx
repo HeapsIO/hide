5845cca39f0f3096d0e69075f367fcc0a72b6569
Nicolas Cannasse <ncannassse@gmail.com>
2020-06-23 10:02:35 +0200

    another minor fix

--- hide/comp/IconTree.hx
+++ hide/comp/IconTree.hx
@@ -128,7 +128,7 @@ class IconTree<T:{}> extends Component {
 				return;
 
 			var node = new Element(event.target).closest("li");
-			if( node == null ) return;
+			if( node == null || node.length == 0 ) return;
    			var i = map.get(node[0].id);
 			if(onDblClick(i.value))
 				return;
