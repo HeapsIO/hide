5dfbcbfedd8df181b283467342bf9bf2d128ea61
ShiroSmith <41334615+ShiroSmith@users.noreply.github.com>
2020-07-31 11:34:03 +0200

    Update Camera prefab

--- hrt/prefab/l3d/Camera.hx
+++ hrt/prefab/l3d/Camera.hx
@@ -5,7 +5,9 @@ import hrt.prefab.Library;
 class Camera extends Object3D {
 
 	var fovY : Float = 45;
-	var zFar : Float = 150;
+	var zFar : Float = 200;
+	var zNear : Float = 0.02;
+	var showFrustum = true;
 
 	public function new(?parent) {
 		super(parent);
@@ -16,6 +18,8 @@ class Camera extends Object3D {
 		var obj : Dynamic = super.save();
 		obj.fovY = fovY;
 		obj.zFar = zFar;
+		obj.zNear = zNear;
+		obj.showFrustum = showFrustum;
 		return obj;
 	}
 
@@ -23,6 +27,75 @@ class Camera extends Object3D {
 		super.load(obj);
 		if(obj.fovY != null) this.fovY = obj.fovY;
 		if(obj.zFar != null) this.zFar = obj.zFar;
+		if(obj.zNear != null) this.zNear = obj.zNear;
+		if(obj.showFrustum != null) this.showFrustum = obj.showFrustum;
+	}
+
+	var g : h3d.scene.Graphics;
+	function drawFrustum( ctx : Context ) {
+
+		if( !showFrustum ) {
+			if( g != null ) {
+				g.remove();
+				g = null;
+			}
+			return;
+		}
+
+		if( g == null ) {
+			g = new h3d.scene.Graphics(ctx.local3d);
+			g.name = "frustumDebug";
+			g.material.mainPass.setPassName("overlay");
+		}
+
+		var c = new h3d.Camera();
+		c.pos.set(0,0,0);
+		c.target.set(1,0,0);
+		c.fovY = fovY;
+		c.zFar = zFar;
+		c.zNear = zNear;
+		c.update();
+
+		var nearPlaneCorner = [c.unproject(-1, 1, 0), c.unproject(1, 1, 0), c.unproject(1, -1, 0), c.unproject(-1, -1, 0)];
+		var farPlaneCorner = [c.unproject(-1, 1, 1), c.unproject(1, 1, 1), c.unproject(1, -1, 1), c.unproject(-1, -1, 1)];
+
+		g.clear();
+		g.lineStyle(1, 0xffffff);
+
+		// Near Plane
+		var last = nearPlaneCorner[nearPlaneCorner.length - 1];
+		g.moveTo(last.x,last.y,last.z);
+		for( fc in nearPlaneCorner ) {
+			g.lineTo(fc.x, fc.y, fc.z);
+		}
+
+		// Far Plane
+		var last = farPlaneCorner[farPlaneCorner.length - 1];
+		g.moveTo(last.x,last.y,last.z);
+		for( fc in farPlaneCorner ) {
+			g.lineTo(fc.x, fc.y, fc.z);
+		}
+
+		// Connections
+		for( i in 0 ... 4 ) {
+			var np = nearPlaneCorner[i];
+			var fp = farPlaneCorner[i];
+			g.moveTo(np.x, np.y, np.z);
+			g.lineTo(fp.x, fp.y, fp.z);
+		}
+
+		// Connections to camera pos
+		g.lineStyle(1, 0xff0000);
+		for( i in 0 ... 4 ) {
+			var np = nearPlaneCorner[i];
+			g.moveTo(np.x, np.y, np.z);
+			g.lineTo(0, 0, 0);
+		}
+	}
+
+	override function updateInstance( ctx, ?p ) {
+		super.updateInstance(ctx, p);
+		drawFrustum(ctx);
 	}
 
 	public function applyTo(c: h3d.Camera) {
@@ -39,6 +112,7 @@ class Camera extends Object3D {
 
 		c.fovY = fovY;
 		c.zFar = zFar;
+		c.zNear = zNear;
 	}
 
 	#if editor
@@ -54,19 +128,29 @@ class Camera extends Object3D {
 			<div class="group" name="Camera">
 				<dl>
 					<dt>Fov Y</dt><dd><input type="range" min="0" max="180" field="fovY"/></dd>
-					<dt>Z Far</dt><dd><input type="range" min="0" max="180" field="zFar"/></dd>
+					<dt>Z Far</dt><dd><input type="range" min="0" max="1000" field="zFar"/></dd>
+					<dt>Z Near</dt><dd><input type="range" min="0" max="10" field="zNear"/></dd>
 					<dt></dt><dd><input class="copy" type="button" value="Copy Current"/></dd>
 					<dt></dt><dd><input class="preview" type="button" value="Preview" /></dd>
 					<dt></dt><dd><input class="reset" type="button" value="Reset" /></dd>
 				</dl>
 			</div>
+			<div class="group" name="Debug">
+				<dl>
+					<dt>Show Frustum</dt><dd><input type="checkbox" field="showFrustum"/></dd>
+				</dl>
+			</div>
 		'),this, function(pname) {
-			var c = ctx.scene.s3d.camera;
-			if(c != null) {
-				c.fovY = fovY;
-				c.zFar = zFar;
-				ctx.scene.editor.cameraController.lockZPlanes = true;
-				ctx.scene.editor.cameraController.loadFromCamera();
+			ctx.onChange(this, pname);
+			if( pname != "showFrustum" ) {
+				var c = ctx.scene.s3d.camera;
+				if(c != null) {
+					c.fovY = fovY;
+					c.zFar = zFar;
+					c.zNear = zNear;
+					ctx.scene.editor.cameraController.lockZPlanes = true;
+					ctx.scene.editor.cameraController.loadFromCamera();
+				}
 			}
 		});
 
@@ -83,6 +167,7 @@ class Camera extends Object3D {
 			this.y = hxd.Math.fmt(cam.pos.y);
 			this.z = hxd.Math.fmt(cam.pos.z);
 			this.zFar = cam.zFar;
+			this.zNear = cam.zNear;
 			this.fovY = cam.fovY;
 			applyTo(cam);
 			ctx.scene.editor.cameraController.lockZPlanes = true;
