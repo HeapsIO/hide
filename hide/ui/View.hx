b69cfcb8e61489ca956f497ee4f3bda62ee52bef
Nicolas Cannasse <ncannasse@gmail.com>
2020-10-17 12:05:26 +0200

    ignore more inputs wrt keystrokes

--- hide/ui/View.hx
+++ hide/ui/View.hx
@@ -101,7 +101,7 @@ class View<T> extends hide.comp.Component {
 			return true;
 		if( active.nodeName == "INPUT" ) {
 			var type = (""+active.getAttribute("type")).toLowerCase();
-			if( type == "text" || type == "null" )
+			if( type == "text" || type == "null" || type == "number" || type == "password" || type == "date" )
 				return true;
 		}
 		return false;
