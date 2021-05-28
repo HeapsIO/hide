caa5a99634761ea790ddbabe3246cceb1c2b1290
Nicolas Cannasse <ncannasse@gmail.com>
2021-05-28 15:17:48 +0200

    added onPrefabLoaded

--- hrt/prefab/Resource.hx
+++ hrt/prefab/Resource.hx
@@ -3,6 +3,7 @@ package hrt.prefab;
 class Resource extends hxd.res.Resource {
 
 	var lib : Prefab;
+	var cacheVersion : Int;
 
 	override function watch( onChanged: Null<Void -> Void> ) {
 		if( entry == null )
@@ -15,17 +16,20 @@ class Resource extends hxd.res.Resource {
 			if( lib != null ) {
 				var data = try haxe.Json.parse(entry.getText()) catch( e : Dynamic ) return; // parsing error (conflict ?)
 				lib.reload(data);
+				onPrefabLoaded(lib);
 			}
 			onChanged();
 		});
 	}
 
 	public function load() : Prefab {
-		if( lib != null )
+		if( lib != null && cacheVersion == CACHE_VERSION )
 			return lib;
 		var data = haxe.Json.parse(entry.getText());
 		lib = Library.create(entry.extension);
 		lib.loadData(data);
+		cacheVersion = CACHE_VERSION;
+		onPrefabLoaded(lib);
 		watch(function() {}); // auto lib reload
 		return lib;
 	}
@@ -35,6 +39,10 @@ class Resource extends hxd.res.Resource {
 		var r = new Resource(null);
 		r.lib = p;
 		return r;
+	}
+
+	public static var CACHE_VERSION = 0;
+	public static dynamic function onPrefabLoaded(p:Prefab) {
 	}
 
 }
