6260ba69f801754a8192a2f5623380ec1b8932d2
Jed974 <48924190+Jed974@users.noreply.github.com>
2021-07-21 17:15:25 +0200

    Shader and Material are now allowed children on Model (#171)

--- hrt/prefab/Model.hx
+++ hrt/prefab/Model.hx
@@ -181,7 +181,7 @@ class Model extends Object3D {
 	override function getHideProps() : HideProps {
 		return {
 			icon : "cube", name : "Model", fileSource : ["fbx","hmd"],
-			allowChildren : function(t) return Library.isOfType(t,Object3D) || ["material", "shader"].indexOf(t) >= 0,
+			allowChildren : function(t) return Library.isOfType(t,Object3D) || Library.isOfType(t,Material) || Library.isOfType(t,Shader),
 			onResourceRenamed : function(f) animation = f(animation),
 		};
 	}
