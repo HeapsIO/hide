HeapsIO/hide

commit 3da8ccc75ec260b0d0fc819db9f73275d881fe35

Emitter: fix Axis alignment mode flipping side, due to euler angles not being normalized. Introduce new align mode ScreenZ useful for screen-aligned polys rotating around Z axis.

hrt/prefab/fx/Emitter.hx
@@ -19,6 +19,7 @@ enum AlignLockAxis {
 	X;
 	Y;
 	Z;
+	ScreenZ;  // Screen-facing flat polygons rotating around Z
 }
 
 enum EmitShape {
@@ -825,11 +826,9 @@ class EmitterObject extends h3d.scene.Object {
 			return;
 
 		if( parent != null ) {
-			invTransform.load(parent.getInvPos());
 
-			if(alignMode == Screen || alignMode == Axis) {
-				var cam = getScene().camera;
-				tmpMat.load(cam.mcam);
+			if(alignMode == Screen) {
+				tmpMat.load(getScene().camera.mcam);
 				tmpMat.invert();
 
 				if(simulationSpace == Local) {  // Compensate parent rotation
@@ -840,19 +839,43 @@ class EmitterObject extends h3d.scene.Object {
 					tmpMat.multiply(tmpMat, tmpMat2);
 				}
 				
-				if(alignMode == Axis) {
-					var a = getEulerAngles(tmpMat);
-					switch alignLockAxis {
-						case X: screenQuat.initRotation(a.x, 0, 0);
-						case Y: screenQuat.initRotation(0, a.y, 0);
-						case Z: screenQuat.initRotation(0, 0, a.z);
-					}
+				screenQuat.initRotateMatrix(tmpMat);
+				tmpQuat.initRotateAxis(1,0,0,Math.PI);  // Flip Y axis so Y is pointing down
+				screenQuat.multiply(screenQuat, tmpQuat);
+			}
+			else if(alignMode == Axis) {
+				var lockAxis = new h3d.Vector();
+				var frontAxis = new h3d.Vector(1, 0, 0);
+				switch alignLockAxis {
+					case X: lockAxis.set(1, 0, 0);
+					case Y: lockAxis.set(0, 1, 0);
+					case Z: lockAxis.set(0, 0, 1);
+					case ScreenZ:
+						lockAxis.set(0, 0, 1);
+						frontAxis.set(0, 1, 0);
 				}
-				else
-					screenQuat.initRotateMatrix(tmpMat);
 
-				if(alignMode == Screen) {
-					tmpQuat.initRotateAxis(1,0,0,Math.PI);  // Flip Y axis so Y is pointing down
+				var lookAtPos = tmpVec;
+				lookAtPos.load(getScene().camera.pos);
+				invTransform.load(parent.getInvPos());
+				lookAtPos.transform(invTransform);
+				var deltaVec = new h3d.Vector(lookAtPos.x - x, lookAtPos.y - y, lookAtPos.z - z);
+	
+				var invParentQ = tmpQuat;
+				invParentQ.initRotateMatrix(invTransform);
+	
+				var targetOnPlane = h3d.col.Plane.fromNormalPoint(lockAxis.toPoint(), new h3d.col.Point()).project(deltaVec.toPoint()).toVector();
+				targetOnPlane.normalize();
+				var angle = hxd.Math.acos(frontAxis.dot(targetOnPlane));
+	
+				var cross = frontAxis.cross(deltaVec);
+				if(lockAxis.dot(cross) < 0)
+					angle = -angle;
+	
+				screenQuat.initRotateAxis(lockAxis.x, lockAxis.y, lockAxis.z, angle);
+				screenQuat.normalize();
+				if(alignLockAxis == ScreenZ) {
+					tmpQuat.initRotateAxis(1,0,0,-Math.PI/2); 
 					screenQuat.multiply(screenQuat, tmpQuat);
 				}
 			}
@@ -1090,7 +1113,7 @@ class Emitter extends Object3D {
 		{ name: "emitSurface", t: PBool, def: false, disp: "Surface", groupName : "Emit Shape" },
 		// ALIGNMENT
 		{ name: "alignMode", t: PEnum(AlignMode), def: AlignMode.None, disp: "Mode", groupName : "Alignment" },
-		{ name: "alignLockAxis", t: PEnum(AlignLockAxis), def: AlignLockAxis.Z, disp: "Lock Axis", groupName : "Alignment" },
+		{ name: "alignLockAxis", t: PEnum(AlignLockAxis), def: AlignLockAxis.ScreenZ, disp: "Lock Axis", groupName : "Alignment" },
 		// COLOR
 		{ name: "useRandomColor", t: PBool, def: false, disp: "Random Color", groupName : "Color" },
 		{ name: "randomColor1", t: PVec(4), disp: "Color 1", def : [0,0,0,1], groupName : "Color" },

commit 299ead6730f5c8a818c6969fa03a6a67fd54a5af

Emitter: fix world speed broken by 3da8ccc75ec260b0d0fc819db9f73275d881fe35

hrt/prefab/fx/Emitter.hx
@@ -822,8 +822,10 @@ class EmitterObject extends h3d.scene.Object {
 		if( emitRate == null || emitRate == VZero )
 			return;
 
-		if( parent != null )
+		if( parent != null ) {
 			parent.getAbsPos().getScale(worldScale);
+			invTransform.load(parent.getInvPos());
+		}
 
 		vecPool.begin();
 
@@ -915,12 +917,12 @@ class EmitterObject extends h3d.scene.Object {
 
 			var lookAtPos = tmpVec;
 			lookAtPos.load(getScene().camera.pos);
-			invTransform.load(parent.getInvPos());
+			var invParent = parent.getInvPos();
 			lookAtPos.transform(invTransform);
 			var deltaVec = new h3d.Vector(lookAtPos.x - x, lookAtPos.y - y, lookAtPos.z - z);
 
 			var invParentQ = tmpQuat;
-			invParentQ.initRotateMatrix(invTransform);
+			invParentQ.initRotateMatrix(invParent);
 
 			var targetOnPlane = h3d.col.Plane.fromNormalPoint(lockAxis.toPoint(), new h3d.col.Point()).project(deltaVec.toPoint()).toVector();
 			targetOnPlane.normalize();