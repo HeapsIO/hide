d346adfbb7f35bbdf2a9765475a1bf3ee6d78cfd
acavalier <acavalie.xlim@gmail.com>
2022-02-22 13:41:55 +0100

    Modified anisotropy shader

--- hrt/shader/AnisotropicForward.hx
+++ hrt/shader/AnisotropicForward.hx
@@ -63,115 +63,181 @@ class NoiseTexture extends hxsl.Shader {
 class AnisotropicForward extends h3d.shader.pbr.DefaultForward {
 	static var SRC = {
 
+		//-----------------------------------------------------------------------------
+		//-- Uniforms -----------------------------------------------------------------
+
 		@global var global : {
             @perObject var modelView : Mat4;
 		};
 
-		var anisotropy : Float;
-		var direction : Vec3;
+		//-----------------------------------------------------------------------------
+		//-- Attributes ---------------------------------------------------------------
 
-		var tangentWorld : Vec3;
-		var bitangentWorld : Vec3;
+		var anisotropy : Float;		/**< Controls the lobe direction */
+		var direction : Vec3;		/**< ? */
+		var tangentWorld : Vec3;	/**< World space   tangent vector */
+		var bitangentWorld : Vec3;	/**< World space bitangent vector */
 
-		function getAnisotropicRoughness( roughness : Float, anisotropy : Float ) : Vec2 {
-			// [Burley12] Offers more pleasant and intuitive results, but is slightly more expensive
-			//var da = sqrt(1 - 0.9 * anisotropy);
-			//var at = anisotropy / da;
-			//var ab = anisotropy * da;
 
-			// [Kulla17] Allows creation of sharp highlights
-			var at = max(roughness * (1.0 + anisotropy), 0.001);
-			var ab = max(roughness * (1.0 - anisotropy), 0.001);
+		//-----------------------------------------------------------------------------
+		//-- Environment textures -----------------------------------------------------
 
+		function getBRDFLUT( roughness : Float, NdV : Float ) : Vec2
+		{
+			return irrLut.get(vec2(roughness, NdV)).rg;
+		}
+
+		function getEnvDiffuse( direction : Vec3 ) : Vec3
+		{
+			return irrDiffuse.get( rotateNormal(direction) ).rgb;
+		}
+
+		function getEnvSpecular( direction : Vec3, roughness : Float ) : Vec3
+		{
+			return textureLod(irrSpecular, rotateNormal(direction), roughness * irrSpecularLevels).rgb;
+		}
+
+		//-----------------------------------------------------------------------------
+		//-- Mappings -----------------------------------------------------------------
+
+		/**
+		 * \brief Returns Anisotropy Roughness mapping
+		 * (Taking into account perceptual roughness mapping)
+		 * 
+		 * From Kulla & Conty in 2017
+		 * "Revisiting Physically Based Shading at Imageworks"
+		 */  
+		function getAnisotropicRoughness( roughness : Float, anisotropy : Float ) : Vec2 
+		{
+			var at = max(roughness*roughness*(1.0 + anisotropy), 0.001);
+			var ab = max(roughness*roughness*(1.0 - anisotropy), 0.001);
 			return vec2(at, ab);
 		}
 
-		function sqr( f : Float ) : Float { return f * f; }
-		function cos_theta( w : Vec3 ) : Float { return w.z; }
-		function cos_2_theta( w : Vec3 ) : Float { return w.z * w.z; }
-		function sin_2_theta( w : Vec3 ) : Float { return max(0., 1. -cos_2_theta(w)); }
-		function sin_theta( w : Vec3 ) : Float { return sqrt(sin_2_theta(w)); }
-		function tan_theta( w : Vec3 ) : Float { return sin_theta(w) / cos_theta(w); }
-		function cos_phi( w : Vec3 ) : Float { return (sin_theta(w) == 0.) ? 1. : clamp(w.x / sin_theta(w), -1., 1.); }
-		function sin_phi( w : Vec3 ) : Float { return (sin_theta(w) == 0.) ? 0. : clamp(w.y / sin_theta(w), -1., 1.); }
 
-		function normalDistributionGGXAnisotropic( omega_h : Vec3, alpha_x : Float, alpha_y : Float ) : Float {
-			var slope_x = -(omega_h.x / omega_h.z);
-			var slope_y = -(omega_h.y / omega_h.z);
-			var denom = (1. + (sqr(slope_x) / sqr(alpha_x)) + (sqr(slope_y) / sqr(alpha_y)) );
-			return 1. / ( ( PI * alpha_x * alpha_y) * (sqr(denom)) ) / sqr(sqr(cos_theta(omega_h)));
+		//-----------------------------------------------------------------------------
+		//-- BRDF - Fresnel -----------------------------------------------------------
+
+		/** \brief Schlick's Fresnel approximation */
+		function fresnelSchlick( VdH : Float, F0 : Vec3 ) : Vec3 
+		{
+			return F0 + (1. - F0) * pow(1. - VdH, 5.);
 		}
 
-		function lambdaGGXAnisotropic( omega : Vec3, alpha_x : Float, alpha_y: Float) : Float {
-			var cos_phi = cos_phi(omega);
-			var sin_phi = sin_phi(omega);
-			var alpha_o = sqrt(sqr(cos_phi) * sqr(alpha_x) + sqr(sin_phi) * sqr(alpha_y));
-			var a = 1. / (alpha_o * tan_theta(omega));
-			return( 0.5 * (-1. + sqrt(1. + 1. / (a*a))) );
+		/** 
+		 * \brief Roughness dependent Fresnel term (Used for indirect lighting) 
+		 * Prevents high specular color at edge for rough surfaces
+		 */
+		function fresnelSchlickWithRoughness( NdV : Float, F0 : Vec3, alpha : Float ) : Vec3 
+		{
+			var Fr = max( vec3(1.0-alpha), F0 ) - F0;
+			return F0 + Fr * pow(1. - NdV, 5.);
 		}
 
-		function fresnelSchlick( wo_dot_wh : Float, F0 : Vec3 ) : Vec3 {
-			return F0 + (1. - F0) * pow(1. - wo_dot_wh, 5.);
+		//-----------------------------------------------------------------------------
+		//-- BRDF - GGX Microfacet Distribution ---------------------------------------
+
+		/** \brief GGX Anisotropic Normal Distribution Function */
+		function GGX_Aniso_NDF(	alpha_t : Float,
+								alpha_b : Float,
+								ToH 	: Float,
+								BoH 	: Float,
+								NoH 	: Float) : Float
+		{
+			var invpi = 1.0/PI;
+			var v = vec3(alpha_b*ToH,alpha_t*BoH, alpha_t*alpha_b*NoH);
+			var v2 = dot(v,v);
+			var a2 = alpha_t*alpha_b;
+			var w2 = a2/v2;
+			return( a2*w2*w2*invpi );
 		}
 
-		function directLighting( lightColor : Vec3, lightDirection : Vec3) : Vec3 {
+		/** \brief GGX Anisotropic Visibility term : corresponds to G / ( 4 * NoV * NoL ) */
+		function GGX_Aniso_Visibility(	alpha_t : Float, alpha_b : Float,
+										ToV 	: Float, BoV 	 : Float, NoV : Float,
+										ToL 	: Float, BoL 	 : Float, NoL : Float) : Float 
+		{
+			var lambdaV = NoL * length(vec3(alpha_t * ToV, alpha_b * BoV, NoV));
+			var lambdaL = NoV * length(vec3(alpha_t * ToL, alpha_b * BoL, NoL));
+			var v = 0.5 / (lambdaV + lambdaL);
+			return clamp(v,0.0,1.0);
+		}
 
-			var NdL = clamp(transformedNormal.dot(lightDirection), 0.0, 1.0);
+		//-----------------------------------------------------------------------------
+		//-- Overrided Functions ------------------------------------------------------
+
+		function directLighting( lightColor : Vec3, lightDirection : Vec3) : Vec3
+		{
 			var result = vec3(0,0,0);
-
-			if( lightColor.dot(lightColor) > 0.0001 && NdL > 0.0 ) {
-
-				var ar = getAnisotropicRoughness(roughness, anisotropy);
-				var at = ar.x;
-				var ab = ar.y;
-
-				// PBRT - Computations are done in tangent space
-				var TBN_t = mat3(tangentWorld, bitangentWorld, transformedNormal);
-				var wo = normalize(view * TBN_t);
-				var wi = normalize(lightDirection * TBN_t);
-				var wg = vec3(0,0,1); // normalize(transformedNormal * TBN_t);
-				var wh = normalize(wo + wi);
-				var wi_dot_wh = clamp(dot(wi, wh),0.,1.); 		// saturate(dot(L,H))
-				var wg_dot_wi = clamp(cos_theta(wi),0.,1.); 	// saturate(dot(N,L))
-
-				var alpha_x = at * at;
-        		var alpha_y = ab * ab;
-				var D = normalDistributionGGXAnisotropic(wh, alpha_x, alpha_y);
-				var F = fresnelSchlick(wi_dot_wh, F0);
-				var lambda_wo = lambdaGGXAnisotropic(wo, alpha_x, alpha_y);
-				var lambda_wi = lambdaGGXAnisotropic(wi, alpha_x, alpha_y);
-				var G = 1. / (1. + lambda_wo + lambda_wi);
-
-				var specular = (D * F * G) / ( 4. * cos_theta(wi) * cos_theta(wo) ) ;
-				var diffuse = albedoGamma / PI * (1.0 - metalness);
-
-				result = (diffuse + specular) * lightColor * wg_dot_wi;
+			/* Checks if is lit */	
+			var NdL = clamp(transformedNormal.dot(lightDirection), 0.0, 1.0);
+			if( lightColor.dot(lightColor) > 0.0001 && NdL > 0.0 ) 
+			{
+				/* Half Vector */
+				var H = normalize(view+lightDirection);
+				/* Normal dot products */
+				var NdH = dot(transformedNormal,H);
+				/* Tangent dot products */
+				var TdH = dot(tangentWorld,H);
+				var TdV = dot(tangentWorld,view);
+				var TdL = dot(tangentWorld,lightDirection);
+				/* Bitangent dot products */
+				var BdH = dot(bitangentWorld,H);
+				var BdV = dot(bitangentWorld,view);
+				var BdL = dot(bitangentWorld,lightDirection);
+				/* Anisotropic roughness mapping */
+				var alphas_aniso = getAnisotropicRoughness(roughness, anisotropy);
+				var alpha_t = alphas_aniso.x;
+				var alpha_b = alphas_aniso.y;
+				/* Fresnel */
+				var VdH = dot(view,H);
+				var F = fresnelSchlick( max(VdH,0.0) , F0 );
+				/* Specular Term */
+				var D = GGX_Aniso_NDF(
+					alpha_t, alpha_b,
+					TdH, BdH, NdH 
+				);
+				var Vis = GGX_Aniso_Visibility(
+					alpha_t, alpha_b,
+					TdV, BdV, NdV,
+					TdL, BdL, NdL
+				);
+				var specular_term = D * F * Vis;
+				/* Diffuse Term */
+				var diffuse_term = albedoGamma / PI;
+				var diffuse_factor = (1.0 - metalness) * (1.0 - F);
+				/* Direct Lighting : Final  */
+				result = NdL * lightColor * (diffuse_factor*diffuse_term + specular_term);
 			}
-
-			return result;
+			return(result);
 		}
 
-		function indirectLighting() : Vec3 {
-
+		function indirectLighting() : Vec3 
+		{
+			/* Perceptual Roughness mapping (alpha = roughness^2) */
+			var alpha = roughness*roughness;
+			/* Bent Normal Indirect Lighting Hack */
 			var anisotropicDirection = anisotropy >= 0.0 ? bitangentWorld : tangentWorld;
 			var anisotropicTangent = cross(anisotropicDirection, view);
 			var anisotropicNormal = cross(anisotropicTangent, anisotropicDirection);
 			var bentNormal = normalize(mix(transformedNormal, anisotropicNormal, anisotropy));
-			var rotatedNormal = rotateNormal(bentNormal);
 			var reflectVec = reflect(-view, bentNormal);
-			var rotatedReflecVec = rotateNormal(reflectVec);
-
-			var F = F0 + (max(vec3(1 - roughness), F0) - F0) * exp2( ( -5.55473 * NdV - 6.98316) * NdV );
-			var diffuse = irrDiffuse.get(rotatedNormal).rgb * albedoGamma;
-			var envSpec = textureLod(irrSpecular, rotatedReflecVec, roughness * irrSpecularLevels).rgb;
-			var envBRDF = irrLut.get(vec2(roughness, NdV));
+			/* Prefiltered Importance Sampling */
+			var envDiff = getEnvDiffuse(bentNormal);
+			var envSpec = getEnvSpecular(reflectVec,alpha);
+			var envBRDF = getBRDFLUT(alpha,NdV);
+			var F = fresnelSchlickWithRoughness( max(NdV,0.0), F0, alpha );
+			/* Combining Diffuse and Specular */
+			var diffuse = envDiff * albedoGamma;
+			var diffuse_factor = (1.0-metalness) * (1.0-F);
 			var specular = envSpec * (F * envBRDF.x + envBRDF.y);
-			var indirect = (diffuse * (1 - metalness) * (1 - F) + specular) * irrPower;
-
+			var indirect = (diffuse_factor*diffuse + specular) * irrPower;
+			/* Final */
 			return indirect * occlusion;
 		}
 
-		function init() {
+		function init() 
+		{
 			view = (cameraPosition - transformedPosition).normalize();
 			NdV = transformedNormal.dot(view).max(0.);
 			var tmp = cross(direction * global.modelView.mat3(), transformedNormal).normalize();
