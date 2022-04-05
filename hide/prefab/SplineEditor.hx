c53be1f96f225fd83d373f33c51ef166c9421a91
Jed <olivier.begue.begue@gmail.com>
2022-04-05 10:56:26 +0200

    Fix add spline point with correct position in refs

--- hide/prefab/SplineEditor.hx
+++ hide/prefab/SplineEditor.hx
@@ -218,14 +218,7 @@ class SplineEditor {
 
 	function addSplinePoint( spd : SplinePointData, ctx : hrt.prefab.Context ) : SplinePoint {
 
-		var invMatrix = new h3d.Matrix();
-		invMatrix.identity();
-		var o : hrt.prefab.Object3D = prefab;
-		while(o != null) {
-			invMatrix.multiply(invMatrix, o.getTransform());
-			o = o.parent.to(hrt.prefab.Object3D);
-		}
-		invMatrix.initInverse(invMatrix);
+		var invMatrix = ctx.local3d.getInvPos();
 
 		var pos = spd.pos.toVector();
 		pos.project(invMatrix);
