06bd3e49ccba11dd5eb40c32f9e83b2515495f44
Clement Espeute <clementespeute@shirogames.com>
2022-10-17 16:23:28 +0200

    Gradient Map Shader

---
+++ hrt/shader/GradientMap.hx
@@ -0,0 +1,14 @@
+package hrt.shader;
+
+class GradientMap extends hxsl.Shader {
+	static var SRC = {
+		@const var USE_ALPHA : Bool;
+		@param var gradient : Sampler2D;
+		var pixelColor : Vec4;
+
+		function fragment() {
+			var t = USE_ALPHA ? pixelColor.a : dot(pixelColor.rgb*pixelColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+			pixelColor.rgb = gradient.get(vec2(t, 0.5)).rgb;
+		}
+	}
+}
