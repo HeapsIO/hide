891fbfb0da9677b9abf3cb1de1f103f4d88a6222
Clement Espeute <clementespeute@shirogames.com>
2022-11-09 10:26:36 +0100

    Extracted BaseTrail shader into own file

---
+++ hrt/shader/BaseTrails.hx
@@ -0,0 +1,39 @@
+package hrt.shader;
+
+class BaseTrails extends hxsl.Shader {
+
+	static var SRC = {
+
+        @param var uvStretch : Float;
+        @const @param var uvRepeat : Int = 0;
+
+        @input var input2 : {
+			var uv : Vec2;
+        };
+
+		var calculatedUV : Vec2;
+
+        function __init__() {
+            calculatedUV = input2.uv;
+        }
+
+        function fragment() {
+            calculatedUV = calculatedUV * vec2(uvStretch, 1.0);
+
+            switch(uvRepeat) {
+                case 0: // Modulo
+                    calculatedUV.x = calculatedUV.x % 1.0;
+                case 1: // Mirror
+                    calculatedUV.x = calculatedUV.x % 2.0;
+                    if (calculatedUV.x > 1.0) {
+                        calculatedUV.x = 2.0-calculatedUV.x;
+                    }
+                case 3: // Clamp
+                    calculatedUV.x = saturate(calculatedUV.x);
+                case 4: {};// None
+                default: {};
+            }
+        }
+	};
+
+}
