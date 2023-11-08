4e0f2b61cfd290680e40802d33ab61ce5e758e1b
Tom Spira <spira.tom@gmail.com>
2023-11-08 11:56:43 +0100

    Fix getSum for VZero evaluator (fix)

--- hrt/prefab/fx/Evaluator.hx
+++ hrt/prefab/fx/Evaluator.hx
@@ -61,7 +61,8 @@ class Evaluator {
 			case VCurve(c): return c.getSum(time);
 			case VAdd(a, b):
 				return getSum(a, time) + getSum(b, time);
-			case VMult(a, b), VZero:
+			case VMult(a, b):
+			case VZero:
 			default: throw "Not implemented";
 		}
 		return 0.0;
