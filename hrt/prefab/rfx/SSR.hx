6a7bb0daf48d2d77fe81e867c6c66ed9a4ce849f
btoth <benoit@shirogames.com>
2023-11-09 13:05:42 +0100

    SSR : - Raymarch in screen space instead of world space

--- hrt/prefab/rfx/SSR.hx
+++ hrt/prefab/rfx/SSR.hx
@@ -3,53 +3,40 @@ package hrt.prefab.rfx;
 class SSRShader extends h3d.shader.ScreenShader {
 	static var SRC = {
 
-		@global var camera : {
-			var inverseViewProj : Mat4;
-			var position : Vec3;
-		};
-
-		@param var cameraViewProj : Mat4;
-
 		@global var depthMap : Channel;
 
+		@param var texSize : Vec2;
 		@param var hdrMap : Sampler2D;
 		@param var roughnessMap : Sampler2D;
 		@param var normalMap : Sampler2D;
 
+		@param var cameraView : Mat4;
+		@param var cameraProj : Mat4;
+		@param var cameraInverseProj : Mat4;
+		@param var cameraPos: Vec3;
+
 		@param var intensity : Float;
 		@param var colorMul : Float;
-		@param var maxRayDistance : Float;
-		@param var startFadeDistance : Float;
-		@const var stepsFirstPass : Int;
-		@const var stepsSecondPass : Int;
 		@param var thickness : Float;
 		@param var maxRoughness : Float;
 		@param var minCosAngle : Float;
-		@param var invGamma : Float;
-
-		function rand(co : Vec2) : Float {
-			return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-		}
+		@param var rayMarchingResolution : Float;
 
 		function reflectedRay(ray : Vec3, normal : Vec3) : Vec3 {
 			return ray - 2.0 * dot(ray, normal) * normal;
 		}
 
-		var curPos : Vec2;
-		function getDepths( worldPos : Vec3) : Vec2 {
-			var projPos = vec4(worldPos, 1.0) * cameraViewProj;
-			curPos = projPos.xy / projPos.w;
-			var distToCam = projPos.z / projPos.w;
-			return vec2(distToCam, depthMap.get(screenToUv(curPos)));
-		}
-
-		function getWorlPos(uv:Vec2):Vec3 {
+		function getViewPos(uv:Vec2):Vec4 {
 			var depth = depthMap.get(uv).r;
 			var ruv = vec4(uvToScreen(uv), depth, 1);
-			var wpos = ruv * camera.inverseViewProj;
-			var result = (wpos.xyz / wpos.w);
-			return result;
+			var vpos = ruv * cameraInverseProj;
+			return vpos / vpos.w;
 		};
+
+		function fromWposToVpos(wpos : Vec3 ) : Vec3 {
+			var vpos = vec4(wpos, 1.0) * cameraView;
+			return vpos.xyz / vpos.w;
+		}
 
 		function fragment() {
 			var normal = normalMap.get(calculatedUV).rgb;
@@ -57,66 +44,57 @@ class SSRShader extends h3d.shader.ScreenShader {
 			if (normal.dot(normal) <= 0)
 				discard;
 
-			var roughnessFactor = 1 - smoothstep(0.0, maxRoughness, roughnessMap.get(calculatedUV).g);
+			 var roughnessFactor = 1 - smoothstep(0.0, maxRoughness, roughnessMap.get(calculatedUV).g);
 			if (roughnessFactor <= 0)
 				discard;
 
-			var depth = depthMap.get(calculatedUV);
+			var positionFrom = getViewPos(calculatedUV);
+			var camDir = normalize(positionFrom.xyz);
+			var viewNormal = normalize(fromWposToVpos(normal + cameraPos));
+			var reflectedRay = reflectedRay(camDir, viewNormal);
+			reflectedRay /= length(reflectedRay.xy);
 
-			var initialWPos = getWorlPos(calculatedUV);
+			var startFrag = positionFrom * cameraProj;
+			startFrag.xyz /= startFrag.w;
+			startFrag.xy = screenToUv(startFrag.xy);
+			startFrag.xy *= texSize;
 
-			var camDir = (initialWPos - camera.position).normalize();
-			var reflectedRay = reflectedRay(camDir, normal);
+			var fragDir = vec4(positionFrom.xyz + reflectedRay, 1.0) * cameraProj;
+			fragDir.xyz /= fragDir.w;
+			fragDir.xy = screenToUv(fragDir.xy);
+			fragDir.xy *= texSize;
+			fragDir.xy = normalize(fragDir.xy - startFrag.xy);
 
-			var curPosWS = initialWPos + reflectedRay * maxRayDistance / stepsFirstPass;
-			var hitFirstPass = false;
+			var hit = 0;
+			var increment = fragDir.xy / saturate(rayMarchingResolution);
+			var frag = startFrag.xy + increment;
+			var uv = frag / texSize;
 
-			for (i in 0 ... stepsFirstPass) {
-				var depths = getDepths(curPosWS);
-				var distToCam = depths.r;
-				var curDepth = depths.g;
-				if (distToCam > curDepth && curPos.x <= 1.0 && curPos.x >= -1.0 && curPos.y <= 1.0 && curPos.y >= -1.0) {
-					var ruv = vec4( curPos, curDepth, 1 );
-					var ppos = ruv * camera.inverseViewProj;
-					var wpos = ppos.xyz / ppos.w;
-					if ((wpos - curPosWS).length() < thickness && dot(normalMap.get(screenToUv(curPos)).rgb, normal) < minCosAngle)
-						hitFirstPass = true;
+			do {
+			  	var positionTo = getViewPos(uv);
+				var viewStepLength = distance(positionTo.xy, positionFrom.xy);
+			  	var viewDistance = positionFrom.z + reflectedRay.z * viewStepLength;
+			  	var depth = viewDistance - positionTo.z;
+
+			  	if (depth >= 0.0 && depth < thickness) {
+					hit = 1;
+					break;
 				}
-				if (hitFirstPass == false) {
-					curPosWS += reflectedRay * maxRayDistance / stepsFirstPass;
-				}
-			}
 
-			if (hitFirstPass == false)
+				frag += increment;
+				uv = frag / texSize;
+
+			} while (uv.x >= 0.0 && uv.x < 1.0 && uv.y >= 0.0 && uv.y < 1.0);
+
+			if (hit != 1)
 				discard;
 
-			curPosWS -= reflectedRay * maxRayDistance / stepsFirstPass / stepsSecondPass;
-			var hitSecondPass = false;
-			for (i in 0 ... stepsSecondPass) {
-				var depths = getDepths(curPosWS);
-				var distToCam = depths.r;
-				var curDepth = depths.g;
-				if (distToCam < curDepth && curPos.x <= 1.0 && curPos.x >= -1.0 && curPos.y <= 1.0 && curPos.y >= -1.0) {
-					var ruv = vec4( curPos, curDepth, 1 );
-					var ppos = ruv * camera.inverseViewProj;
-					var wpos = ppos.xyz / ppos.w;
-					if ((wpos - curPosWS).length() < thickness)
-						hitSecondPass = true;
-				}
-				if (hitSecondPass == false) {
-					curPosWS -= reflectedRay * maxRayDistance / stepsFirstPass / stepsSecondPass;
-				}
-			}
+			var reflectionNormal = normalMap.get(uv).rgb;
+			if (dot(reflectionNormal, normal) > minCosAngle || reflectionNormal.dot(reflectionNormal) <= 0)
+				discard;
 
-			var fragmentColor = vec3(0.0);
-			var alpha = 0.0;
-			if (hitFirstPass && hitSecondPass) {
-				fragmentColor = hdrMap.getLod(screenToUv(curPos), 0.0).rgb;
-				alpha = 1.0 - saturate(((curPosWS - initialWPos).length() - startFadeDistance) / (maxRayDistance - startFadeDistance));
-				alpha *= intensity;
-			}
-
-			pixelColor.rgba = saturate(vec4(fragmentColor * colorMul, alpha * roughnessFactor));
+			var fragmentColor = hdrMap.get(uv).rgb;
+			pixelColor = saturate(vec4(fragmentColor * colorMul, intensity * roughnessFactor));
 		}
 	}
 }
@@ -130,17 +108,12 @@ class SSR extends RendererFX {
 
 	@:s public var intensity : Float = 1.;
 	@:s public var colorMul : Float = 1.;
-	@:s public var maxRayDistance : Float = 10.;
-	@:s public var startFadeDistance : Float = 5.;
-	@:s public var steps : Int = 10;
-	@:s public var stepsFirstPass : Int = 10;
-	@:s public var stepsSecondPass : Int = 10;
 	@:s public var thickness : Float = 1.0;
 	@:s public var blurRadius : Float = 1.0;
-	@:s public var randomPower : Float = 0.0;
 	@:s public var textureSize : Float = 0.5;
 	@:s public var maxRoughness : Float = 0.75;
-	@:s public var minAngle : Float = 15;
+	@:s public var minAngle : Float = 5.0;
+	@:s public var rayMarchingResolution : Float = 0.5;
 
 	function new(?parent) {
 		super(parent);
@@ -159,18 +132,19 @@ class SSR extends RendererFX {
 
 			var normalMap = r.ctx.getGlobal("normalMap").texture;
 			ssrShader.normalMap = normalMap;
-
+			var t = r.ctx.engine.getCurrentTarget();
+			ssrShader.texSize = new h3d.Vector((t == null ? r.ctx.engine.width : t.width), (t == null ? r.ctx.engine.height : t.height));
 			ssrShader.colorMul = colorMul;
 			ssrShader.intensity = intensity;
-			ssrShader.maxRayDistance = maxRayDistance;
-			ssrShader.startFadeDistance = startFadeDistance;
-			ssrShader.stepsFirstPass = stepsFirstPass;
-			ssrShader.stepsSecondPass = stepsSecondPass;
 			ssrShader.thickness = thickness;
 			ssrShader.maxRoughness = maxRoughness;
 			ssrShader.minCosAngle = Math.cos(hxd.Math.degToRad(minAngle));
+			ssrShader.rayMarchingResolution = rayMarchingResolution;
 
-			ssrShader.cameraViewProj = r.ctx.camera.m;
+			ssrShader.cameraView = r.ctx.camera.mcam;
+			ssrShader.cameraProj = r.ctx.camera.mproj;
+			ssrShader.cameraInverseProj = r.ctx.camera.getInverseProj();
+			ssrShader.cameraPos = r.ctx.camera.pos;
 
 			ssrPass.setGlobals(r.ctx);
 
@@ -196,11 +170,8 @@ class SSR extends RendererFX {
 				<dt>Color Mul</dt><dd><input type="range" min="0" max="1" field="colorMul"/></dd>
 				<dt>Max Roughness</dt><dd><input type="range" min="0" max="1" field="maxRoughness"/></dd>
 				<dt>Min Angle</dt><dd><input type="range" min="0" max="90" field="minAngle"/></dd>
-				<dt>Max ray distance</dt><dd><input type="range" min="0" max="10" field="maxRayDistance"/></dd>
-				<dt>Start Fade Distance</dt><dd><input type="range" min="0" max="10" field="startFadeDistance"/></dd>
-				<dt>Steps first pass</dt><dd><input type="range" min="1" max="30" step="1" field="stepsFirstPass"/></dd>
-				<dt>Steps second pass</dt><dd><input type="range" min="1" max="20" step="1" field="stepsSecondPass"/></dd>
 				<dt>Thickness</dt><dd><input type="range" min="0" max="1" field="thickness"/></dd>
+				<dt>Ray marching resolution</dt><dd><input type="range" min="0" max="1" field="rayMarchingResolution"/></dd>
 				<dt>Blur radius</dt><dd><input type="range" min="0" max="5" field="blurRadius"/></dd>
 				<dt>Texture size</dt><dd><input type="range" min="0" max="1" field="textureSize"/></dd>
 			</dl>
