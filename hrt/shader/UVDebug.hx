2f1b197b948151feec9a2954487217a47beae658
Clement Espeute <clementespeute@shirogames.com>
2023-11-20 16:06:29 +0100

    Restored uvdebug

--- hrt/shader/UVDebug.hx
+++ hrt/shader/UVDebug.hx
@@ -18,19 +18,19 @@ class UVDebug extends hxsl.Shader {
 
         function fragment() {
 			var uv = calculatedUV;
-			{
-				var a = uv.x;
-				a += 0.5;
-				uv.x = a;
-				uv.y = a;
-			}
+			// {
+			// 	var a = uv.x;
+			// 	a += 0.5;
+			// 	uv.x = a;
+			// 	uv.y = a;
+			// }
 
-			{
-				var a = uv.y;
-				a += 0.5;
-				uv.x = a;
-				uv.y = a;
-			}
+			// {
+			// 	var a = uv.y;
+			// 	a += 0.5;
+			// 	uv.x = a;
+			// 	uv.y = a;
+			// }
 
             pixelColor.rgb = vec3(uv.x % 1.0, uv.y, 0.0);
             /*if (abs(calculatedUV.x % 1.0) < 0.05)
