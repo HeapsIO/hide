f045d3cd05ff33c09a55a9d5ceea1d6def57194f
lviguier <leo.vgr@hotmail.com>
2024-01-23 11:11:19 +0100

    shadergraph: fix dissolve node

--- hrt/shgraph/nodes/Dissolve.hx
+++ hrt/shgraph/nodes/Dissolve.hx
@@ -10,19 +10,17 @@ class Dissolve extends ShaderNodeHxsl {
 
 	static var SRC = {
 		@sginput var rgba : Vec4;
-		@sginput(calculatedUV) var uv : Vec2;
-		@sginput var dissolveMap : Sampler2D;
+		@sginput var dissolveMap : Vec4;
 		@sginput(0.5) var progress : Float;
 		@sginput(0.5) var saturation : Float;
 		@sginput(1.0) var width : Float;
 		@sgoutput var output : Vec4;
 
 		function fragment() {
-			var pix = dissolveMap.get(uv);
 			var edge = mix(1.0 + width, -width, progress);
-			var ramp = saturate((1.0 + saturation) * (width - abs(edge - pix.r)) / width);
+			var ramp = saturate((1.0 + saturation) * (width - abs(edge - dissolveMap.r)) / width);
 			output.rgb = rgba.rgb;
-			output.a = rgba.a * ramp * pix.a;
+			output.a = rgba.a * ramp * dissolveMap.a;
 		}
 	};
 }
