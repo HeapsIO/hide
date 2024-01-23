6ecf01e98753acfd4e5fc08ef9df3ee2d9a40d92
lviguier <leo.vgr@hotmail.com>
2024-01-23 15:06:59 +0100

    trails : rollback changes

--- hrt/prefab/l3d/Trails.hx
+++ hrt/prefab/l3d/Trails.hx
@@ -470,10 +470,10 @@ class TrailObj extends h3d.scene.Mesh {
 
 		updateParams();
 
-		if (trails != null) {
-			var pos = this.getAbsPos();
-			addPoint(trails[0], pos.tx, pos.ty, pos.tz, ECamera, 1.0);
-		}
+		// if (trails != null) {
+		// 	var pos = this.getAbsPos();
+		// 	addPoint(trails[0], pos.tx, pos.ty, pos.tz, ECamera, 1.0);
+		// }
 	}
 
 	#if editor
