831875e2bfbaefa4a26884e5716e3623d460aaf9
Nicolas Cannasse <ncannasse@gmail.com>
2024-01-28 11:45:22 +0100

    minor

--- hide/Element.hx
+++ hide/Element.hx
@@ -2,7 +2,7 @@ package hide;
 
 typedef Element = #if hl hltml.JQuery #else js.jquery.JQuery #end;
 typedef Event = #if hl hltml.Event #else js.jquery.Event #end;
-typedef HTMLElement = #if hl hltml.Dom #else js.html.Element #end;
+typedef HTMLElement = #if hl hltml.Element #else js.html.Element #end;
 
 function getVal( e : Element ) {
 	return #if js e.val() #else e.getValue() #end;
