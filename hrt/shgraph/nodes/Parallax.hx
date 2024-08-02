60ff724423fec4d0319cf512603bfa0cc33ce935
clementlandrin <clement.landrin@hotmail.fr>
2024-08-02 15:31:22 +0200

    Adding Parallax shader graph node.

---
+++ hrt/shgraph/nodes/Parallax.hx
@@ -0,0 +1,36 @@
+package hrt.shgraph.nodes;
+
+using hxsl.Ast;
+
+@name("Parallax")
+@description("Parallaxed uv offset")
+@group("UV")
+@width(100)
+class Parallax extends ShaderNodeHxsl {
+
+	static var SRC = {
+		@sginput("camera.position") var cameraPosition : Vec3;
+		@sginput(1.0) var range : Float;
+		@sgoutput var output : Vec2;
+		
+		@global var global : {
+			var modelView : Mat4;
+		};
+
+		@input var input : {
+			var tangent : Vec3;
+        };
+
+		var transformedPosition : Vec3;
+		var transformedNormal : Vec3;
+		function fragment() {
+			var viewWS = (cameraPosition - transformedPosition).normalize();
+			var tanX = input.tangent * global.modelView.mat3();
+			var tanY = normalize(transformedNormal.cross(tanX));
+			var viewNS = vec3(viewWS.dot(tanX), viewWS.dot(tanY), viewWS.dot(transformedNormal)).normalize();
+
+			output = viewNS.xy * range;
+		}
+	};
+
+}
