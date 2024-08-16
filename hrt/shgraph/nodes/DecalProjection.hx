50937ff669fd02f1875dd9a5e2e8c4d00917ea15
TothBenoit <benoit.tothdenimal@gmail.com>
2024-08-16 14:47:23 +0200

    ShaderGraph : Added World UV option to decal projection

--- hrt/shgraph/nodes/DecalProjection.hx
+++ hrt/shgraph/nodes/DecalProjection.hx
@@ -3,13 +3,16 @@ package hrt.shgraph.nodes;
 using hxsl.Ast;
 
 @name("Decal Projection")
-@description("Apply decal projection on UVs")
+@description("Compute decal projection")
 @group("UV")
 class DecalProjection extends ShaderNodeHxsl {
 
+	@prop() var isWorldUV : Bool = false;
+
 	static var SRC = {
-		@sginput("calculatedUV") var uv : Vec2;
 		@sgoutput var output : Vec2;
+
+		@sgconst var isWorldUV : Int;
 
         @global var camera : {
 			var inverseViewProj : Mat4;
@@ -29,9 +32,41 @@ class DecalProjection extends ShaderNodeHxsl {
             var depth = depthMap.get(screenToUv(screenPos));
             var ruv = vec4( screenPos, depth, 1 );
             var wpos = ruv * matrix;
-            var lpos = (wpos.xyz / wpos.w);
-            output = lpos.xy + 0.5;
+			if ( isWorldUV == 1 ) {
+				var ppos = ruv * camera.inverseViewProj;
+				output = ppos.xy / ppos.w;
+			} else {
+				var lpos = (wpos.xyz / wpos.w);
+				output = lpos.xy + 0.5;
+			}
 		}
 	};
+	
+	override function getConstValue(name: String) : Null<Int> {
+		switch (name) {
+			case "isWorldUV":
+				return isWorldUV ? 1 : 0;
+			default:
+				return null;
+		}
+	}
+
+	#if editor
+	override public function getPropertiesHTML(width : Float) : Array<hide.Element> {
+		var elements = super.getPropertiesHTML(width);
+		var container = new hide.Element('<div style="width: ${width * 0.8}px; height:40px"></div>');		
+		container.append('<span>&nbspWorld UV</span>');
+		var worldUVEl = new hide.Element('<input type="checkbox" id="isWorldUV"></input>');
+		worldUVEl.prop('checked', this.isWorldUV);		
+		container.append(worldUVEl);
+	
+		worldUVEl.on("change", function(e) {
+			this.isWorldUV = worldUVEl.is(':checked');
+		});
+
+		elements.push(container);
+		return elements;
+	}
+	#end
 
 }
