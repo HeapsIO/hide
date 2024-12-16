8a558673b67a133bc646d44faf68d1ebfe31abf6
TothBenoit <benoit.tothdenimal@gmail.com>
2024-12-16 17:20:21 +0100

    Fix typo in ScreenToWorld.hx

--- hrt/shgraph/nodes/ScreenToWorld.hx
+++ hrt/shgraph/nodes/ScreenToWorld.hx
@@ -11,10 +11,10 @@ class ScreenToWorld extends ShaderNodeHxsl {
 		@sginput var input : Vec3;
 		@sgoutput var output : Vec3;
 
-		@global var camera : { var invViewProj : Mat4; };
+		@global var camera : { var inverseViewProj : Mat4; };
 
 		function fragment() {
-			var result = vec4(input, 1.0) * camera.invViewProj;
+			var result = vec4(input, 1.0) * camera.inverseViewProj;
 			output = result.xyz / result.w;
 		}
 	};
