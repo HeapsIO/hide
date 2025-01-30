e1731b6a3b7bfe97a30d68802807b5ca32aa9ef6
Clément Espeute <espeut.clement@gmail.com>
2025-01-30 11:41:57 +0100

    [animgraph] added resolver parameter to res.loadAnim()

--- hrt/animgraph/Resource.hx
+++ hrt/animgraph/Resource.hx
@@ -40,8 +40,8 @@ class Resource extends hxd.res.Resource {
 		return animGraph;
 	}
 
-	public function loadAnim() : AnimGraphInstance {
-		return load().getAnimation();
+	public function loadAnim(resolver: hrt.animgraph.AnimGraphInstance.AnimResolver = null) : AnimGraphInstance {
+		return load().getAnimation(resolver);
 	}
 
 	public static var CACHE_VERSION = 0;
