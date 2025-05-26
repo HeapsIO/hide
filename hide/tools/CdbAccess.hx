db5679d401d3382cbe649770e2d4a63432cc0b9f
Nicolas Cannasse <ncannasse@gmail.com>
2025-05-26 16:07:35 +0200

    added resolve()

--- hide/tools/CdbAccess.hx
+++ hide/tools/CdbAccess.hx
@@ -3,10 +3,12 @@ package hide.tools;
 class CdbAccess<T,Kind> {
 
 	public var all(get,never) : cdb.Types.ArrayRead<T>;
+	var name : String;
 	var map : Map<String,T> = [];
 	var lines : Array<T>;
 
 	public function new( sheet : String ) {
+		this.name = sheet;
 		lines = hide.Ide.inst.getCDBContent(sheet);
 		var s = hide.Ide.inst.database.getSheet(sheet);
 		if( s.idCol != null ) {
@@ -24,6 +26,21 @@ class CdbAccess<T,Kind> {
 
 	public function get( kind : Kind ) : T {
 		return map.get(cast kind);
+	}
+
+	public function resolve( id : String, ?opt : Bool, ?approximate : Bool ) : T {
+		var v = map.get(id);
+		if( v == null && approximate ) {
+			id = id.toLowerCase();
+			var best = 1000;
+			for( k => value in map ) {
+				if( StringTools.startsWith(k.toLowerCase(),id) && k.length < best ) {
+					v = value;
+					best = k.length;
+				}
+			}
+		}
+		return v == null && !opt ? throw "Missing " + name + "." + id : v;
 	}
 
 }
