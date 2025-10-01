d4d226aeb8b5c6c65b4ed03e90d3f7260b8c28d1
Clément Espeute <espeut.clement@gmail.com>
2025-10-01 18:00:12 +0200

    [kit] Undo/Redo Poc

--- hide/kit/Properties.hx
+++ hide/kit/Properties.hx
@@ -3,11 +3,15 @@ package hide.kit;
 class Properties extends Element {
 	public var editedPrefabsProperties : Array<Properties> = [];
 	var prefab : hrt.prefab.Prefab;
+	var prefabUndoPoint : Dynamic = null;
+	var edit : hide.prefab.EditContext;
+
 	var registeredElements : Map<String, Element> = [];
 
-	public function new(properties: hide.kit.Properties, parent: Element, id: String, prefab: hrt.prefab.Prefab ) {
+	public function new(properties: hide.kit.Properties, parent: Element, id: String, prefab: hrt.prefab.Prefab, editContext: hide.prefab.EditContext ) {
 		super(properties, parent, id);
 		this.prefab = prefab;
+		edit = editContext;
 	}
 
 	public function register(element: Element) {
@@ -17,9 +21,17 @@ class Properties extends Element {
 	public function broadcastValueChange(input: Input<Dynamic>, isTemporaryEdit: Bool) {
 		var idPath = input.getIdPath();
 
+		if (prefabUndoPoint == null) {
+			prefabUndoPoint = prefab.save();
+			for (childProperties in editedPrefabsProperties) {
+				childProperties.prefabUndoPoint = childProperties.prefab.save();
+			}
+		}
+
 		input.onValueChange(isTemporaryEdit);
 
 		for (childProperties in editedPrefabsProperties) {
+
 			var childElement = childProperties.registeredElements.get(idPath);
 			var childInput = Std.downcast(childElement, Type.getClass(input));
 
@@ -27,6 +39,39 @@ class Properties extends Element {
 				childInput.value = input.value;
 				childInput.onValueChange(isTemporaryEdit);
 			}
+		}
+
+		if (!isTemporaryEdit) {
+			var sideEffects : Array<(isUndo:Bool) -> Void> = [];
+			createUndoStep(sideEffects);
+
+			for (childProperties in editedPrefabsProperties) {
+				childProperties.createUndoStep(sideEffects);
+			}
+
+			if (sideEffects.length > 0) {
+				edit.properties.undo.change(Custom((isUndo: Bool) -> {
+					for (sideEffect in sideEffects) {
+						sideEffect(isUndo);
+					}
+				}));
+			}
+		}
+	}
+
+	function createUndoStep(sideEffects : Array<(isUndo:Bool) -> Void>) : Void {
+		var before = prefabUndoPoint;
+		prefabUndoPoint = null;
+		var after = prefab.save();
+		if (hrt.prefab.Diff.diff(before, after) != Skip) {
+			sideEffects.push((isUndo) -> {
+				if (isUndo) {
+					prefab.load(before);
+				} else {
+					prefab.load(after);
+				}
+				prefab.updateInstance();
+			});
 		}
 	}
 }
