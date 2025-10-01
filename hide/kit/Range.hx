e418f7e731061a936461d2a8922311e33bf012f1
Clément Espeute <espeut.clement@gmail.com>
2025-10-01 17:30:51 +0200

    [kit] Multi edit poc

--- hide/kit/Range.hx
+++ hide/kit/Range.hx
@@ -1,2 +1,9 @@
 package hide.kit;
 
+class Range extends Slider {
+	public function new(properties: Properties, parent: Element, id: String, min: Float, max: Float) {
+		super(properties, parent, id);
+		this.min = min;
+		this.max = max;
+	}
+}
