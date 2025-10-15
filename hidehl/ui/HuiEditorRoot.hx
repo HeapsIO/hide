68722745bcf05109915699b0460e419111a68d27
Clément Espeute <espeut.clement@gmail.com>
2025-10-15 14:13:55 +0200

    [kit] Fix hl compilation

--- hidehl/ui/HuiEditorRoot.hx
+++ hidehl/ui/HuiEditorRoot.hx
@@ -1,14 +1,14 @@
 package hidehl.ui;
 
-class HuiEditorRoot extends Element {
+class HuiEditorRoot extends HuiElement {
 	static var SRC =
 		<hui-editor-root>
-			<element id="panel-left" public></element>
-			<element id="panel-middle" public>
+			<hui-element id="panel-left" public></hui-element>
+			<hui-element id="panel-middle" public>
 				// <scene id="scene-main" public/>
-			</element>
-			<element id="panel-right" public>
-			</element>
+			</hui-element>
+			<hui-element id="panel-right" public>
+			</hui-element>
 		</hui-editor-root>
 
 	public function new(?parent: h2d.Object) {
