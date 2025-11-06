12d51fbff9c37268baeb05989f4e7bbafc22b9f9
TothBenoit <benoit.tothdenimal@gmail.com>
2025-11-06 16:46:31 +0100

    HideKit : add edit2 to DiskSpawn

--- hrt/prefab/fx/gpuemitter/DiskSpawn.hx
+++ hrt/prefab/fx/gpuemitter/DiskSpawn.hx
@@ -34,6 +34,15 @@ class DiskSpawn extends SpawnShader {
 		sh.maxRadius = maxRadius;
 	}
 
+	override function edit2( ctx : hrt.prefab.EditContext2 ) {
+		ctx.build(
+			<category("Spawn")>
+				<slider min={0.01} field={minRadius}/>
+				<slider min={0.01} field={maxRadius}/>
+			</category>
+		);
+	}
+
 	#if editor
 	override function edit( ctx : hide.prefab.EditContext ) {
 		ctx.properties.add(new hide.Element('
