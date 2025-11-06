d19266a982b89c53c93aeca3fb76742b686cf21e
Yuxiao Mao <yuxiao.mao@outlook.com>
2025-11-06 14:24:53 +0100

    HideKit: add edit2 to hrt.prefab.l3d.Level3D

--- hrt/prefab/l3d/Level3D.hx
+++ hrt/prefab/l3d/Level3D.hx
@@ -6,6 +6,10 @@ package hrt.prefab.l3d;
 @:deprecated("Use hrt.prefab.Library instead")
 class Level3D extends Object3D
 {
+	override function edit2(ctx:hrt.prefab.EditContext2) {
+		super.edit2(ctx);
+	}
+
 	#if editor
 	override function getHideProps() : hide.prefab.HideProps {
 		return { icon : "sitemap", name : "Level3D", allowParent: _ -> false};
