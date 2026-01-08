28aa372b43048d2efc69f2df51703c6aec993e2d
Clément Espeute <espeut.clement@gmail.com>
2026-01-08 12:49:24 +0100

    [hide_hl] HideHL hello world

--- hide/Element.hx
+++ hide/Element.hx
@@ -1,8 +1,8 @@
 package hide;
 
-typedef Element = #if hl hltml.JQuery #else js.jquery.JQuery #end;
-typedef Event = #if hl hltml.Event #else js.jquery.Event #end;
-typedef HTMLElement = #if hl hltml.Element #else js.html.Element #end;
+typedef Element = #if hl Dynamic #else js.jquery.JQuery #end;
+typedef Event = #if hl Dynamic #else js.jquery.Event #end;
+typedef HTMLElement = #if hl Dynamic #else js.html.Element #end;
 
 function getVal( e : Element ) {
 	return #if js e.val() #else e.getValue() #end;
