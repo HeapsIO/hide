a7ccd96db22f28e3c02e1160aa0410da5ef187da
LeoVgr <leo.vgr@hotmail.com>
2026-06-23 10:38:30 +0200

    HideHL/TextureViewer: add channels and uncompresssed / compressed view

--- hide_hl/hide/view/Texture.hx
+++ hide_hl/hide/view/Texture.hx
@@ -2,7 +2,7 @@ package hide.view;
 import hrt.ui.*;
 
 #if hui
-private class ImageViewerShader extends hxsl.Shader {
+private class TextureViewerShader extends hxsl.Shader {
 
 	static var SRC = {
 
@@ -69,6 +69,7 @@ class Texture extends HuiView<{path: String}> {
 	
 	public var bmp : h2d.Bitmap;
 	// public var camCtrl : h2d.CameraController;
+	var shader : TextureViewerShader;
 
 	public function new(_state: Dynamic, ?parent) {
 		super(_state, parent);
@@ -80,6 +81,8 @@ class Texture extends HuiView<{path: String}> {
 		this.huiBg.setTexture(tex);
 		this.huiBg.imageMode = CssParser.BackgroundImageMode.Repeat;
 		var tex = HuiRes.loader.load(TRANSPARENT_TEX_PATH).toImage().toTexture();
+
+		shader = new TextureViewerShader();
 
 		load(state.path);
 
@@ -119,50 +122,114 @@ class Texture extends HuiView<{path: String}> {
 		helpBtn.onClick = (_) -> {
 			uiBase.addPopup(new hrt.ui.HuiToolbar.HuiHelpPopup(this.registeredCommands), { object: Element(helpBtn), directionX: StartInside, directionY: EndOutside });
 		};
-		new HuiIcon("question_mark", helpBtn);
-		widgets.push(helpBtn);
+		
+		var compressedBtn = new HuiToggle();
+		compressedBtn.dom.addClass("group-start");
+		compressedBtn.toggled = true;
+		new HuiIcon("raw_off", compressedBtn);
+		widgets.push(compressedBtn);
+
+		var uncompressedBtn = new HuiToggle();
+		uncompressedBtn.dom.addClass("group");
+		new HuiIcon("raw", uncompressedBtn);	
+		widgets.push(uncompressedBtn);
+
+		var compareBtn = new HuiToggle();
+		compareBtn.dom.addClass("group-end");
+		new HuiIcon("compare", compareBtn);	
+		widgets.push(compareBtn);
+
+		compressedBtn.onClick = (_) -> {
+			if (compressedBtn.toggled) return;
+			compressedBtn.toggled = !compressedBtn.toggled;
+			uncompressedBtn.toggled = false;
+			compareBtn.toggled = false;
+			shader.comparisonFactor = 1;
+		}
+
+		uncompressedBtn.onClick = (_) -> {
+			if (uncompressedBtn.toggled) return;
+			uncompressedBtn.toggled = !uncompressedBtn.toggled;
+			compressedBtn.toggled = false;
+			compareBtn.toggled = false;
+			shader.comparisonFactor = 0;
+		}
+
+		compareBtn.onClick = (_) -> {
+			if (compareBtn.toggled) return;
+			compareBtn.toggled = !compareBtn.toggled;
+			uncompressedBtn.toggled = false;
+			compressedBtn.toggled = false;
+		}
 
 		var rChannelBtn = new HuiToggle();
+		rChannelBtn.toggled = true;
 		rChannelBtn.dom.addClass("group-start");
 		new HuiText("R", rChannelBtn);
 		rChannelBtn.onClick = (_) -> {
-			
+			rChannelBtn.toggled = !rChannelBtn.toggled;
+			setChannelVisible(0, rChannelBtn.toggled);
 		}
 		widgets.push(rChannelBtn);
 
 		var gChannelBtn = new HuiToggle();
+		gChannelBtn.toggled = true;
 		gChannelBtn.dom.addClass("group");
 		new HuiText("G", gChannelBtn);
 		gChannelBtn.onClick = (_) -> {
-			
+			gChannelBtn.toggled = !gChannelBtn.toggled;
+			setChannelVisible(1, gChannelBtn.toggled);
 		}
 		widgets.push(gChannelBtn);
 
 		var bChannelBtn = new HuiToggle();
+		bChannelBtn.toggled = true;
 		bChannelBtn.dom.addClass("group");
 		new HuiText("B", bChannelBtn);
 		bChannelBtn.onClick = (_) -> {
-			
+			bChannelBtn.toggled = !bChannelBtn.toggled;
+			setChannelVisible(2, bChannelBtn.toggled);
 		}
 		widgets.push(bChannelBtn);
 
 		var aChannelBtn = new HuiToggle();
+		aChannelBtn.toggled = true;
 		aChannelBtn.dom.addClass("group-end");
 		new HuiText("A", aChannelBtn);
 		aChannelBtn.onClick = (_) -> {
-			
+			aChannelBtn.toggled = !aChannelBtn.toggled;
+			setChannelVisible(3, aChannelBtn.toggled);
 		}
 		widgets.push(aChannelBtn);
+
+		new HuiIcon("question_mark", helpBtn);
+		widgets.push(helpBtn);
 
 		return widgets;
 	}
 
 	function load(path : String) {
-		if (bmp == null)
+		if (bmp == null) {
 			bmp = new h2d.Bitmap(null, this);
+			bmp.addShader(shader);
+			for (idx in 0...4)
+				setChannelVisible(idx, true);
+			shader.comparisonFactor = 1;
+		}
 
-		var tex = hxd.res.Loader.currentInstance.load(Ide.inst.getRelPath(path)).toImage().toTexture();
-		bmp.tile = h2d.Tile.fromTexture(tex);
+		var compressedTex = hxd.res.Loader.currentInstance.load(Ide.inst.getRelPath(path)).toImage().toTexture();
+		bmp.tile = h2d.Tile.fromTexture(compressedTex);
+		
+		var bytes = sys.io.File.getBytes(path);
+		var uncompressedTex = hxd.res.Any.fromBytes(path, bytes).toImage().toTexture();
+
+		shader.compressedTex = compressedTex;
+		shader.uncompressedTex = uncompressedTex;
+	}
+
+	function setChannelVisible(channelIdx : Int, visible : Bool) {
+		shader.channels &= ~(1 << channelIdx);
+		if (visible) shader.channels |= 1 << channelIdx;
 	}
 }
 #end
